d577e19c7e11490566bbd600fed1525823d9967f
Leelw <156767901+LeeWanyanga@users.noreply.github.com>
2024-05-30 16:59:11 +0300

    Update and rename Subscriptions.js to Ticket.js

---
+++ Frontend/src/pages/Ticket.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidenav from "../Sidenav";
+import { Carousel } from 'react-bootstrap';
+import { Typography } from "@mui/material";
+
+const Ticket = () => {
+    return ( 
+        <div>
+        <p>welcome to ticket lorem ipsum yaatata</p>
+        <Typography></Typography>
+        </div>
+     );
+}
+ 
+export default Ticket;
