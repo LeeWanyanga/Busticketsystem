dcc142c8e28367b3d602e2486c9d8c4b5b2e7c80
Leelw <156767901+LeeWanyanga@users.noreply.github.com>
2024-05-30 16:58:45 +0300

    Update and rename Shorts.js to Profile.js

---
+++ Frontend/src/pages/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidenav from "../Sidenav";
+import { Carousel } from 'react-bootstrap';
+
+
+const Profile = () => {
+    return ( 
+        <div>
+        <p>welcome to Profile lorem ipsum yaatata</p>
+        
+        </div>
+     );
+}
+ 
+export default Profile;
