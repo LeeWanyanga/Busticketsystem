f8067e4191ea1bd510e6a2c5c3b08deb6a21110d
Leelw <156767901+LeeWanyanga@users.noreply.github.com>
2024-05-30 16:57:19 +0300

    Update Contact.js

--- Frontend/src/pages/About.js
+++ Frontend/src/pages/About.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Sidenav from "../Sidenav";
+import { Carousel } from 'react-bootstrap';
+import Typography from '@mui/material/Typography';
 
 
-const About = () => {
+const Contact = () => {
     return ( 
-        <>
-        <p>welcome to About lorem ipsum yaatata</p>
-        
-        </>
+        <div>
+        <h1><u>Feel Free to Contact us regarding more information that you need</u></h1>
+        <Typography>Nairobi Branch</Typography>
+        <h2></h2>
+        </div>
      );
 }
  
-export default About;
+export default Contact;
